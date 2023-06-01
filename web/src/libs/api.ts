2f9704a2561fa8ecda810374497cbc1ea8b7dd78
rockske8er <rockske8er@gmail.com>
2023-06-01 16:58:20 -0300

    feat((web)):chore: prod env

--- web/src/libs/api.ts
+++ web/src/libs/api.ts
@@ -1,3 +1,8 @@
 import axios from 'axios'
 
-export const api = axios.create({ baseURL: 'http://192.168.1.6:3333' })
+// prod => https://spacetime-fwwg.onrender.com
+
+// dev =>   'http://192.168.1.6:3333'
+export const api = axios.create({
+  baseURL: 'https://spacetime-fwwg.onrender.com',
+})
